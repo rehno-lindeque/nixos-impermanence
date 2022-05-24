d4b5c8a798c2099e12fed58769105f0ed2abf431
Rehno Lindeque <errantkid@gmail.com>
2022-05-24 09:57:08 -0400

    Add a check

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs"; # /nixos-22.05
+
+    # Input is used for checks
+    impermanence.url = "github:nix-community/impermanence";
   };
   outputs = {
     self,
     nixpkgs,
+    impermanence,
   }: let
     lib = nixpkgs.lib;
   in {
@@ -18,4 +22,25 @@
         ];
       };
     };
+
+    checks.x86_64-linux.example =
+      (lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          impermanence.nixosModules.impermanence
+          self.nixosModules.default
+          {
+            environment.automaticPersistence = {
+              normal.path = "/persistence";
+            };
+            boot.loader.systemd-boot.enable = true;
+            fileSystems."/".device = "none";
+          }
+        ];
+      })
+      .config
+      .system
+      .build
+      .toplevel;
+  };
 }
